eeebab6f5a669ddf7eebaefda3f1d36906bace2a
Rianda Rafqi <riandarafqi321@gmail.com>
2025-02-13 14:34:43 +0700

    commit

---
+++ pertemuan2/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    private int nilai;
+    private Node next;
+    
+    // inisialisasi Node
+    public Node (int nilai) {
+        this.nilai = nilai;
+    }
+
+    // Setter & Getter
+}
